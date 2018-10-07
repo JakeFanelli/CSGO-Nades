5397e9ef6040a1e6f33b06a58c3c9e0d6d529d84
Jake <jxf6552@rit.edu>
2018-10-07 18:15:27 -0400

    stairs smoke

--- pages/maps/js/js.js
+++ pages/maps/js/js.js
@@ -1,3 +1,11 @@
 function navBar(){
   	$("#nav-bar").load("navbar.html");
 }
+function genModal(title, video){
+	var title = title;
+	var body = video;
+	var video = document.getElementById('videoTag');
+	$('.modal-title').html(title);
+	video.setAttribute("src",body);
+	video.load();
+}
