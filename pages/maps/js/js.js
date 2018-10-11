bc956ada0992cbc392c11cafb492d40c36a7c527
Jake <jxf6552@rit.edu>
2018-10-10 21:09:49 -0400

    dont need movs?

--- pages/maps/js/js.js
+++ pages/maps/js/js.js
@@ -6,15 +6,12 @@ function genModal(title, video){
 	var body = video;
 	console.log(video);
 	var body1 = body.concat(".webm");
-	var body2 = body.concat(".mov");
 	var body3 = body.concat(".mp4");
 	console.log(body1);
-	console.log(body2);
 	console.log(body3);
 	var video = document.getElementById('videoTag');
 	$('.modal-title').html(title);
 	video.setAttribute("src",body1);
 	video.setAttribute("src",body2);
-	video.setAttribute("src",body3);
 	video.load();
 }
