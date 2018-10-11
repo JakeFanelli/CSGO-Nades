92781b2e6bf2a022574770d2887920c547d98877
Jake <jxf6552@rit.edu>
2018-10-11 17:31:39 -0400

    refactoring video code

--- pages/maps/js/js.js
+++ pages/maps/js/js.js
@@ -4,12 +4,8 @@ function navBar(){
 function genModal(title, video){
 	var title = title;
 	var body = video;
-	console.log(video);
-	var bodyWebm = body.concat(".webm");
-	var bodyMp4 = body.concat(".mp4");
 	var video = document.getElementById('videoTag');
 	$('.modal-title').html(title);
-	video.setAttribute("src",bodyWebm);
-	video.setAttribute("src",bodyMp4);
+	video.setAttribute("src",body);
 	video.load();
 }
