cdcf4ceda6c7968496d10a8644eb18d50065667a
Jake <jxf6552@rit.edu>
2018-10-10 21:01:13 -0400

    testing for all browsers

--- pages/maps/js/js.js
+++ pages/maps/js/js.js
@@ -4,8 +4,17 @@ function navBar(){
 function genModal(title, video){
 	var title = title;
 	var body = video;
+	console.log(video);
+	var body1 = body.concat(".webm");
+	var body2 = body.concat(".mov");
+	var body3 = body.concat(".mp4");
+	console.log(body1);
+	console.log(body2);
+	console.log(body3);
 	var video = document.getElementById('videoTag');
 	$('.modal-title').html(title);
-	video.setAttribute("src",body);
+	video.setAttribute("src",body1);
+	video.setAttribute("src",body2);
+	video.setAttribute("src",body3);
 	video.load();
 }
