5baac18635dd937e34360d9b7951182221a01b87
Jake <jxf6552@rit.edu>
2018-10-20 20:13:26 -0400

    Filter bar

--- pages/maps/js/js.js
+++ pages/maps/js/js.js
@@ -9,3 +9,85 @@ function genModal(title, video){
 	video.setAttribute("src",body);
 	video.load();
 }
+var smokeClicked = false;
+var flashClicked = false;
+var molotovClicked = false;
+
+function smokeClick(){
+	var smoke = document.getElementById('smokeButton');
+	var flash = document.getElementById('flashButton');
+	var molotov = document.getElementById('molotovButton');
+	if(smokeClicked===false){//if not clicked already
+		smoke.setAttribute("style","background-color: #000");
+		smokeClicked=true;
+		$('.smokes').show();
+		if(flashClicked == false){
+			$('.flashes').hide();
+		}
+		if(molotovClick == false){
+			$('.molotovs').hide();
+		}
+	}else{//if clicked already
+		smoke.setAttribute("style","background-color: #222");
+		smokeClicked=false;
+		if(flashClicked==false&&molotovClicked==false){
+			$('.smokes').show();
+			$('.flashes').show();
+			$('.molotovs').show();
+		}else{
+			$('.smokes').hide();
+		}
+	}
+}
+function flashClick(){
+	var smoke = document.getElementById('smokeButton');
+	var flash = document.getElementById('flashButton');
+	var molotov = document.getElementById('molotovButton');
+	if(flashClicked===false){//if not clicked already
+		flash.setAttribute("style","background-color: #000");
+		flashClicked=true;
+		$('.flashes').show();
+		if(smokeClicked == false){
+			$('.smokes').hide();
+		}
+		if(molotovClick == false){
+			$('.molotovs').hide();
+		}
+	}else{//if clicked already
+		flash.setAttribute("style","background-color: #222");
+		flashClicked=false;
+		if(smokeClicked==false&&molotovClicked==false){
+			$('.smokes').show();
+			$('.flashes').show();
+			$('.molotovs').show();
+		}else{
+			$('.flashes').hide();
+		}
+	}
+}
+function molotovClick(){
+	var smoke = document.getElementById('smokeButton');
+	var flash = document.getElementById('flashButton');
+	var molotov = document.getElementById('molotovButton');
+	if(molotovClicked===false){//if not clicked already
+		molotov.setAttribute("style","background-color: #000");
+		molotovClicked=true;
+		$('.molotovs').show();
+		if(smokeClicked == false){
+			$('.smokes').hide();
+		}
+		if(flashClicked == false){
+			$('.flashes').hide();
+		}
+	}else{//if clicked already
+		molotov.setAttribute("style","background-color: #222");
+		molotovClicked=false;
+		if(flashClicked==false&&smokeClicked==false){
+			$('.smokes').show();
+			$('.flashes').show();
+			$('.molotovs').show();
+		}else{
+			$('.molotovs').hide();
+		}
+	}
+}
